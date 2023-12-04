80b4b4a99e584f79eb0902da929ccb8abd8afa42
Mohamed Hamas <e0726186@u.nus.edu>
2023-12-04 21:33:10 +0800

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import {
+  createBrowserRouter,
+  Outlet,
+  RouterProvider,
+} from "react-router-dom";import Home from "./pages/Home.jsx"
+import ResponsiveAppBar from './components/ResponsiveAppBar';
+import Footer from './components/Footer';
+
+const Layout = () => {
+  return (
+    <>
+    <ResponsiveAppBar></ResponsiveAppBar>
+    <Home></Home>
+    <Footer></Footer>
+    </>
+  )
+  }
+  
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="container">
+      <Layout></Layout>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
