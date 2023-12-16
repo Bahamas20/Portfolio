3ca9d45a9d15e0559a13a014077c72071defb337
Mohamed Hamas <e0726186@u.nus.edu>
2023-12-17 00:24:32 +0800

    animation for about photo

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,9 @@ function About() {
     <div className='section about'>
         <h1 className='section_title font-mont'>About Me</h1>
             <div className='description'>
+            <div className='box'>
             <img className='about_img' src={image}></img>
+            </div>
             <h3 className='text font-open'>
             Hello, I'm Hamas, an undergraduate Computer Science student at NUS. My focus areas are Artificial Intelligence (AI) and Databases, as I'm passionate about the potential of AI to transform industries and the critical role that databases play in managing and analyzing data. I am a determined individual with a strong willingness to learn and motivation to achieve excellence.
             <br />
